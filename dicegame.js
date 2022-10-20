7213770db8f0d3d40f50fa03a8ecab355f2d64df
Connor Fuller <ConnorJFuller@gmail.com>
2022-10-20 16:13:52 -0500

    first commit

---
+++ dicegame.js
@@ -0,0 +1,45 @@
+let dice1 = []
+let dice2 = []
+let dice3 = []
+let dice4 = []
+let dice5 = []
+
+diceScore = 0
+var roundScore = 0
+var rollScore = 0
+
+//might be able to make this into a class
+let playerRoll = (dice1) => {
+    dice1.Math.floor(Math.random() * 7)
+    {}
+}
+let playerRoll2 = (dice2) => {
+    dice2.Math.floor(Math.random() * 7)
+}
+let playerRoll3 = (dice3) => {
+    dice3.Math.floor(Math.random() * 7)
+}
+
+let playerRoll4 = (dice4) => {
+    dice4.Math.floor(Math.random() * 7)
+}
+let playerRoll5 = (dice5) => {
+    dice5.Math.floor(Math.random() * 7)
+}
+
+
+let playerScore = (playerRoll) =>{
+    //have to combine the 5 dice rolls into score
+}
+
+/* 1's are worth 100 points
+5's are worth 50 points
+triple 1's are 1000
+triple 2-6's are worth face value *100 (2=200, 3=300, 6=600 ect)
+show a round score
+show a total score 
+
+
+2,3,4,6, DO NOT GIVE POINTS ON THEIR OWN, THEYH AVE TO HAVE */
+
+
